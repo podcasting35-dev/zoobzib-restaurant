acfdf7712682f11bc6f0cd88133f6ddab0c972f1
podcasting35-dev <podcasting35@gmail.com>
2026-01-02 14:29:52 +0530

    Component

---
+++ Navbar.js
@@ -0,0 +1,11 @@
+
+export default function Navbar(){
+ return (
+  <nav>
+   <a href='/'>Home</a>
+   <a href='/menu'>Menu</a>
+   <a href='/order'>Order</a>
+   <a href='/contact'>Contact</a>
+  </nav>
+ );
+}
